10ae05b61a372bf2106206cb27083f35e3e5435b
Swarnim Doegar <swarnim@imagekit.io>
2026-04-23 02:58:18 +0530

    use output: 'static' as 'hybrid' output is deprecated

--- test-app/astro.config.mjs
+++ test-app/astro.config.mjs
@@ -3,7 +3,7 @@ import node from '@astrojs/node';
 import imagekit from '@imagekit/astro/integration';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   integrations: [imagekit()],
   adapter: node({
     mode: 'standalone',
